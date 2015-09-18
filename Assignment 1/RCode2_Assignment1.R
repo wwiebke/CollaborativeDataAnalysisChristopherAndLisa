18117e73e3c3bcdab05c811539731860f606459a
Christopher Cosler <christopher@cosler.net>
2015-09-18 15:29:32 +0200

    Introduce the student's sleep data

---
+++ Assignment 1/RCode2_Assignment1.R
@@ -0,0 +1,12 @@
+###########################
+# R-File for Assignment 1 dealing with dataset 1 (sleep)
+###########################
+
+#Find a full list of pre-installed datasets here
+#https://stat.ethz.ch/R-manual/R-devel/library/datasets/html/00Index.html
+
+#Set working directory Christopher
+setwd("C:/Users/Christopher/OneDrive/Dokumente/Studium/Hertie School of Governance/Courses/Fall 2015/Collaborative Data Analysis/Project 1")
+
+#Load student's sleep data
+data("sleep")
